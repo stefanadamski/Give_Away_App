756ab1cd6cd961ccf1b4fbb6fa7a2488c77b6c93
stefanadamski <72260121+stefanadamski@users.noreply.github.com>
2021-05-15 21:05:13 +0200

    mpm3

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,25 +1,23 @@
-import './css/App.css';
-import logo from './images/Form-Hero-Image.jpg'
+import React from "react";
+import './css/Home.css';
+import Home from './components/Home.js'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/location" component={Location} />
+        </Switch>
+      </Router>
   );
 }
 
 export default App;
+
+
