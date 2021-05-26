de3e02168d1bd253845e4470d7a8039fad8db14e
stefanadamski <72260121+stefanadamski@users.noreply.github.com>
2021-05-26 13:10:04 +0200

    CSS

--- my_app/src/components/homeComponents/EasySteps/EasySteps.js
+++ my_app/src/components/homeComponents/EasySteps/EasySteps.js
@@ -6,9 +6,34 @@ const EasySteps = () => {
         <div className="stepsContainer">
             <h1 className='stepsTitle'> Wystarczą 4 proste kroki </h1>
             <div className='stepsDecoration'> </div>
-            <div className='stepsElements'> Nori grape silver beet broccoli kombu beet greens
-                fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek
-                lentil turnip greens parsnip. </div>
+            <div className='stepsElements'>
+                <div className='boxElement'>
+                    <div className='boxIcon boxIcon1'> </div>
+                    <h1> Wybierz rzeczy </h1>
+                    <div className='boxLine'> </div>
+                    <p> ubrania, zabawki, sprzęt i inne </p>
+                </div>
+                <div className='boxElement'>
+                    <div className='boxIcon boxIcon2'> </div>
+                    <h1> Spakuj je </h1>
+                    <div className='boxLine'> </div>
+                    <p> skorzystaj z worków na śmieci </p>
+                </div>
+                <div className='boxElement'>
+                    <div className='boxIcon boxIcon3'> </div>
+                    <h1> Zdecyduj komu <br/>
+                        chcesz pomóc </h1>
+                    <div className='boxLine'> </div>
+                    <p> wybierz zaufane miejsce </p>
+                </div>
+                <div className='boxElement'>
+                    <div className='boxIcon boxIcon4'> </div>
+                    <h1> Zamów kuriera </h1>
+                    <div className='boxLine'> </div>
+                    <p> kurier przyjedzie w dogodnym terminie </p>
+                </div>
+            </div>
+            <div className='giveAwayButton'> Oddaj rzeczy </div>
         </div>
     );
 };
